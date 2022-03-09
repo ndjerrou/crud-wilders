4256b562377e5603c02bab14b6d7ed579cb5ce73
ummahway <contact@ummaway.com>
2022-03-09 11:42:03 +0100

    CRUD Wilders

--- index.js
+++ index.js
@@ -1,25 +1,25 @@
-const express = require('express') // a bare path
-const mongoose = require('mongoose')
-const morgan = require('morgan')
+const express = require("express"); // a bare path
+const mongoose = require("mongoose");
+const morgan = require("morgan");
 
-const connect = require("./utilities/connect")
-const wilder = require("./models/Wilder")
+const connect = require("./utilities/connect");
+const wilder = require("./models/Wilder");
 
- const wilderRouter = require('./ressources/wilder/wilder.controller')
+const wilderRouter = require("./ressources/wilder/wilder.controller");
 
-const app = express()
+const app = express();
 // app.use((req, res, next)=>{
 //     console.log('kikoo, je suis un middleware')
 //     next()
 // })
 
-app.use(morgan('dev'))
+app.use(morgan("dev"));
 
-app.use('/api/wilders', wilderRouter)
+app.use("/api/wilders", wilderRouter);
 
-connect()
+connect();
 
-
-
-const PORT = 3000
-app.listen(PORT, ()=>console.log(`Listenning on port ${PORT}`))
+const PORT = 3000;
+app.listen(process.env.PORT || PORT, () =>
+  console.log(`Listenning on port ${PORT}`)
+);
